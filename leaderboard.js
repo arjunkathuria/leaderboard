3bb23625689da53750f67d889083b479e3fb5d8d
Arjun Kathuria <arjun.kathuria8@gmail.com>
2015-07-21 12:54:50 +0530

    phase 1 complete, advance features start
    part 1 of the app complete with basic features, now adding of advance
    features starts

--- leaderboard.js
+++ leaderboard.js
@@ -1,2 +1,49 @@
-PlayersList = new Mongo.Collection("players");
-console.log('Hello World!');
+PlayersList = new Mongo.Collection('players');
+
+
+if(Meteor.isClient){
+  Template.leaderboard.helpers({
+    'player': function(){
+      return PlayersList.find({}, {sort: {score: -1,name: 1}});
+    },
+    'selectedClass': function () {
+      var playerId = this._id;
+      var selectedPlayer = Session.get('selectedPlayer');
+      if (selectedPlayer===playerId){
+      return "selected";}
+    },
+    'showSelectedPlayer': function () {
+      var selectedPlayer = Session.get('selectedPlayer');
+      return PlayersList.findOne(selectedPlayer);
+    }
+  });
+}
+//leaderboard events start
+if(Meteor.isClient){
+  Template.leaderboard.events({
+    'click .player': function () {
+      var playerId = this._id;
+      Session.set('selectedPlayer',playerId);
+    },
+    'click .increment': function(){
+       var selectedPlayer = Session.get('selectedPlayer');
+       PlayersList.update(selectedPlayer,{$inc: {score: 5}});
+    },
+    'click .decrement':function () {
+      var selectedPlayer = Session.get('selectedPlayer');
+      PlayersList.update(selectedPlayer,{$inc: {score: -5}});
+    }
+  });
+}
+//leaderboard events end !
+
+//addplayer events start
+if(Meteor.isClient){
+  Template.addPlayers.events({
+    'submit form': function(event){
+        event.preventDefault();
+        var playerNameVar = event.target.playerName;
+        console.log(playerNameVar);
+    }
+  });
+}
